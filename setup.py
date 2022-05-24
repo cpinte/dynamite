1e1ea2ed3bd97e1a394e0345e9604905643fec95
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2022-05-24 15:01:57 +0200

    adding setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='co_layers',
+      description='Infer the molecular layers of a protoplanetary disk',
+      url='http://github.com/cpinte/CO_layers',
+      author='Christophe Pinte',
+      license='MIT',
+      packages=['co_layers'],
+      zip_safe=False)
